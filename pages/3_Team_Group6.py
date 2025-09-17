fc30845bf20f54499f1f6366c554f385f1964af4
Sancandoit <sanchit.thapa@gmail.com>
2025-09-17 13:40:43 +0400

    Create 3_Team_Group6.py

---
+++ pages/3_Team_Group6.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("👥 Team Group 6")
+
+st.markdown("""
+**Presented by:**  
+- Sanchit Singh Thapa  
+- Soniya Arunkumar  
+- Chandraveer Singh  
+- Pritish Dhal  
+""")
+
+st.success("Proud contributors to the AgriFarm E-Business Strategy Project (SBR 2).")
