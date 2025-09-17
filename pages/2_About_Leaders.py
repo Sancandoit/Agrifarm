a1d627f73fe750dbdbe37b60f4e3027b1301bfe5
Sancandoit <sanchit.thapa@gmail.com>
2025-09-17 13:40:24 +0400

    Create 2_About_Leaders.py

---
+++ pages/2_About_Leaders.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("👩‍💼 Visionary Leaders Behind Makro PRO")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("https://www.mckinsey.com/~/media/McKinsey/Our%20People/Noppamas-Sivakriskul.jpg", width=250)
+    st.subheader("Noppamas Sivakriskul")
+    st.write("**Senior Partner and Managing Partner, Bangkok**")
+    st.caption("Leads McKinsey’s digital business-building work in Southeast Asia and advises B2B and B2C companies across the continent.")
+
+with col2:
+    st.image("https://www.mckinsey.com/~/media/McKinsey/Our%20People/Simon-Wintels.jpg", width=250)
+    st.subheader("Simon Wintels")
+    st.write("**Partner, Amsterdam**")
+    st.caption("Helps consumer-goods and retail clients capture growth through excellence in strategy, digital and advanced analytics, and marketing.")
